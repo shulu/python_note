87277c097f38bd86df4e3852099fcc53cc45aa27
shulu <qq961085397@163.com>
2017-09-28 23:23:55 +0800

    2017-09-28
    Signed-off-by: shulu <qq961085397@163.com>

--- awesome_python/README.md
+++ awesome_python/README.md
@@ -15,3 +15,26 @@
 *  +- ios/                  <-- 存放iOS App工程
 *  |
 *  +- LICENSE               <-- 代码LICENSE
+
+### 后端API包括：
+* 获取日志：```GET /api/blogs```
+* 创建日志：``` POST /api/blogs```
+* 修改日志：``` POST /api/blogs/:blog_id ```
+* 删除日志：``` POST /api/blogs/:blog_id/delete ```
+* 获取评论：``` GET /api/comments ```
+* 创建评论：``` POST /api/blogs/:blog_id/comments ```
+* 删除评论：``` POST /api/comments/:comment_id/delete ```
+* 创建新用户：```` POST /api/users ```
+* 获取用户：``` GET /api/users ```
+###管理页面包括：
+* 评论列表页：``` GET /manage/comments ```
+* 日志列表页：``` GET /manage/blogs ```
+* 创建日志页：``` GET /manage/blogs/create ```
+* 修改日志页：``` GET /manage/blogs/ ```
+* 用户列表页：``` GET /manage/users ```
+### 用户浏览页面包括：
+* 注册页：``` GET /register ```
+* 登录页：``` GET /signin ```
+* 注销页：``` GET /signout ```
+* 首页：``` GET / ```
+* 日志详情页：``` GET /blog/:blog_id ```
--- awesome_python/www/handlers.py
+++ awesome_python/www/handlers.py
@@ -81,12 +81,7 @@ def cookie2user(cookie_str):
 
 @get('/')
 async def index(request):
-    summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-    blogs = [
-        Blog(id='1', name='Test Blog', summary=summary, created_at=time.time()-120),
-        Blog(id='2', name='Something New', summary=summary, created_at=time.time()-3600),
-        Blog(id='3', name='Learn Swift', summary=summary, created_at=time.time()-7200)
-    ]
+    blogs = await Blog.findAll('', '', orderBy='created_at desc')
     return {
         '__template__': 'blogs.html',
         'blogs': blogs
@@ -94,15 +89,16 @@ async def index(request):
 
 
 @get('/blog/{id}')
-def get_blog(id):
-    blog = yield from Blog.find(id)
-    comments = yield from Comment.findAll('blog_id=?', [id], orderBy='created_at desc')
+async def get_blog(id):
+    blog = await Blog.find(id)
+    comments = await Comment.findAll('blog_id=?', [id], orderBy='created_at desc')
     for c in comments:
         c.html_content = text2html(c.content)
     blog.html_content = markdown2.markdown(blog.content)
+    logging.info(blog)
     return {
-        '__template__': 'blog.html',
-        'blog': blog,
+        '__template__': 'blogs.html',
+        'blogs': [blog],
         'comments': comments
     }
 
