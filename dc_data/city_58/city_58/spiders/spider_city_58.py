a5ebf4aa2918e0718c5252f5dd51aa547b011295
shulu <qq961085397@163.com>
2018-02-05 22:56:30 +0800

    2017-02-05

---
+++ dc_data/city_58/city_58/spiders/spider_city_58.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class SpiderCity58Spider(scrapy.Spider):
+    name = 'spider_city_58'
+    allowed_domains = ['58.com']
+    start_urls = ['http://58.com/']
+
+    def parse(self, response):
+        print('im in 解析器')
