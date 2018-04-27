a314a9f543aa98079012a4798194d49d8dc2ee62
舒露 <shulubanggood>
2018-04-27 10:33:49 +0800

    shulu commit

--- process/process.py
+++ process/process.py
@@ -3,3 +3,9 @@
 import os
 
 print('Process (%s) start...' % os.getppgid())
+
+pid = os.fork()
+if pid == 0:
+    print('I am child process (%s) and my parent is %s.' % (os.getpid(), os.getppid()))
+else:
+    print('I (%s) just created a child process (%s).' % (os.getpid(), pid))
