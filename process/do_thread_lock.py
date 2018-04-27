d7b9e314e3e738c82a10908a5262ad27bc8eb3a3
舒露 <shulubanggood>
2018-04-27 16:33:25 +0800

    shulu commit

--- process/do_thread_lock.py
+++ process/do_thread_lock.py
@@ -18,8 +18,8 @@ def run_thread(n):
     for i in range(100000):
         change_it(n)
 
-t1 = threading.Thread(target=run_thread, args=(5,))
-t2 = threading.Thread(target=run_thread, args=(8,))
+t1 = threading.Thread(target=run_thread, args=(10,))
+t2 = threading.Thread(target=run_thread, args=(20,))
 t1.start()
 t2.start()
 t1.join()
