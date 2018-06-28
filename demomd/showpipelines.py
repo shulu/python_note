306e1669ef301d41f7a81d005d96f551dd0fa784
舒露 <shulubanggood>
2018-06-28 16:36:15 +0800

    shulu commit

---
+++ demomd/showpipelines.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'SarcasMe'
+
+import requests
+import json
+import math
+
+url = 'https://g.banggood.com/banggood/www/pipelines.json?scope=all&page=1&private_token=s64NyxXzb-Dreu9-4iM3'
+#url = 'https://g.banggood.com/users/shulu/projects?private_token=s64NyxXzb-Dreu9-4iM3'
+ret = requests.get(url)
+data = json.loads(ret.text)
+
+all_beta_commit = data['count']['all']
+pages = math.ceil(all_beta_commit/20)
+print("pipeline_id | author |     author_email      |  push branch |  now status ")
+for pipe in data['pipelines']:
+    pipeline_id = str(pipe['id'])
+    author = pipe['commit']['author_name']
+    author_email = pipe['commit']['author_email']
+    branch = 'unknow'
+    if pipe['ref']['branch']:
+        branch = pipe['ref']['name']
+    pipeline_status = pipe['details']['status']['label']
+    print('    ' + pipeline_id + '  | ' + author + '  | ' + author_email + ' | ' + branch + ' | ' + pipeline_status)
