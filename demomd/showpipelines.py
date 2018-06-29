026ff213e41175723b4276412b73b3649d35963d
舒露 <shulubanggood>
2018-06-29 14:49:40 +0800

    爬取pipeline完成 后续做数据分析
    Signed-off-by: 舒露 <shulubanggood>

--- demomd/showpipelines.py
+++ demomd/showpipelines.py
@@ -8,29 +8,83 @@ import json
 import math
 from openpyxl import load_workbook
 
-url = 'https://g.banggood.com/banggood/www/pipelines.json?scope=all&page=1&private_token=s64NyxXzb-Dreu9-4iM3'
-#url = 'https://g.banggood.com/users/shulu/projects?private_token=s64NyxXzb-Dreu9-4iM3'
-ret = requests.get(url)
-data = json.loads(ret.text)
 
-all_beta_commit = data['count']['all']
-pages = math.ceil(all_beta_commit/20)
-# print("pipeline_id | author |     author_email      |  push branch |  now status ")
-print("pipeline_id |   author |  push branch |  now status ")
-for pipe in data['pipelines']:
-    pipeline_id = str(pipe['id'])
-    author = pipe['commit']['author_name']
-    author_email = pipe['commit']['author_email']
-    commit_message = pipe['commit']['title']
-    branch = 'unknow'
-    if pipe['ref']['branch']:
-        branch = pipe['ref']['name']
-    pipeline_status = pipe['details']['status']['label']
-    # print('    ' + pipeline_id + '  | ' + author + '  | ' + author_email + ' | ' + branch + ' | ' + pipeline_status)
-    print('    ' + pipeline_id + '  |   ' + author + ' |   ' + branch + '    | ' + pipeline_status)
+class my_pipelines():
 
-#https://www.cnblogs.com/sun-haiyu/p/7096423.html
-#wb = load_workbook('pipelines.xlse')
-#sheet = wb.get_sheet_by_name('Sheet')
-#sheet['A1'] = 'Hello world!'
-#print(sheet['A1'].value)
+    def __init__(self):
+
+        self.project = 'banggood/www'
+        self.private_token = 's64NyxXzb-Dreu9-4iM3'
+        self.excel_name = 'pipelines.xlsx'
+
+    def get_pipelines(self):
+
+        data = self.return_pipelines(False)
+        all_commit = data['count']['all']
+        self.record_max(all_commit)
+        pages = int(math.ceil(all_commit/20))
+
+        while pages > 0:
+            print('start page '+str(pages))
+            data = self.return_pipelines(pages)
+            pipelines_data = []
+            # pipeline_id	author	author_email	branch	commit_id	commit_message	commit_date pipeline_status
+            for pipe in data['pipelines']:
+
+                pipeline_id = str(pipe['id'])
+                author = pipe['commit']['author_name']
+                commit_id = pipe['commit']['id']
+                commit_date = pipe['created_at']
+                author_email = pipe['commit']['author_email']
+                commit_message = pipe['commit']['title']
+                branch = 'unknow'
+                if pipe['ref']['branch']:
+                    branch = pipe['ref']['name']
+                pipeline_status = pipe['details']['status']['label']
+                pipeline = [
+                    pipeline_id, author, author_email, branch,
+                    commit_id, commit_message, commit_date, pipeline_status
+                ]
+                pipelines_data.append(pipeline)
+                # print('    ' + pipeline_id + '  |   ' + author + ' |   ' + branch + '    | ' + pipeline_status)
+            self.save_to_excel(pipelines_data)
+            pages -= 1
+
+    def return_pipelines(self, page):
+
+        if page:
+            url = 'https://g.banggood.com/' + self.project + '/pipelines.json?scope=all&page='+str(page)+'&private_token=' + self.private_token
+        else:
+            url = 'https://g.banggood.com/' + self.project + '/pipelines.json?scope=all&page=1&private_token=' + self.private_token
+        ret = requests.get(url)
+        if ret.status_code == 200:
+            data = json.loads(ret.text)
+            return data
+        else:
+            return False
+
+    def save_to_excel(self, datas):
+
+        wb = load_workbook(self.excel_name)
+        sheet = wb.active
+        row = sheet.max_row  # <-最大行数
+        max_row = row + 1
+        lens = len(datas)
+        for i in range(0, lens-1):
+            for col in range(1, len(datas[i]) + 1):
+                # print(max_row, col1)
+                _ = sheet.cell(row=max_row, column=col, value=str(datas[i][col - 1]))
+            max_row += 1
+            wb.save(filename=self.excel_name)
+        # print("保存成功")
+
+    @staticmethod
+    def record_max(count):
+        with open("max_count.json", "w") as f:
+            json.dump({'all_count':count}, f)
+        print("加载入文件完成...")
+
+if __name__ == '__main__':
+
+    pipe = my_pipelines()
+    pipe.get_pipelines()
