cc53b6ec7e0cd79ad2616a9a57e2eab2a6bae03e
舒露 <shulubanggood>
2018-07-06 10:14:56 +0800

    shulu commit

--- demomd/usexlsx.py
+++ demomd/usexlsx.py
@@ -7,16 +7,33 @@ import pandas as pd
 import numpy as np
 
 
-df = pd.DataFrame(pd.read_excel('pipelines.xlsx'))
+df = pd.DataFrame(pd.read_excel('pipelines_bak.xlsx'))
 
 # data = df.loc[df['author'].isin(['李俊波']), ['pipeline_id', 'author', 'commit_date']].pipeline_id.count()
 # data = df.loc[df['author']=='李俊波', ['pipeline_id', 'author', 'commit_date']].pipeline_id.count()
+# ['周海东' '余亚' '李俊波' '卢仕幸' '朱志明' '周佳献' '李阳阳' '严伟明' '杨智富' '邓永军' '杨水荣' '谢鼎杰' '陈首全']
+author = '吴明聪'
+first_commit = df.loc[df['author'] == author, ['commit_id']].iloc[[0]].commit_id.values[0]
+last_commit = df.loc[df['author'] == author, ['commit_id']].iloc[[-1]].commit_id.values[0]
+used_branch = df.loc[df['author'] == author, ['branch']].drop_duplicates().branch.values
+count_branch = df.loc[df['author'] == author, ['branch', 'pipeline_id']].groupby('branch')['pipeline_id'].count().values
+print("now author: %s" % author)
+print("author first commit: %s" % first_commit)
+print("author last commit: %s" % last_commit)
+for i in range(0, len(used_branch)):
+    print('%s commit to branch %s %d times' % (author, used_branch[i], count_branch[i]) )
+print('-----------------------------------------------------------')
+exit()
 authors = df['author'].unique()
 for author in authors:
 
-    first_commit = df.loc[df['author']==author, ['commit_id']].iloc[[0]].commit_id
-    last_commit = df.loc[df['author']==author, ['commit_id']].iloc[[-1]].commit_id
-    print(first_commit)
-    print(last_commit)
+    first_commit = df.loc[df['author'] == author, ['commit_id']].iloc[[0]].commit_id.values[0]
+    last_commit = df.loc[df['author'] == author, ['commit_id']].iloc[[-1]].commit_id.values[0]
+    used_branch = df.loc[df['author'] == author, ['branch']].drop_duplicates().branch.values
+    count_branch = df.loc[df['author'] == author, ['branch', 'pipeline_id']].groupby('branch')['pipeline_id'].count().values
     print("now author: %s" % author)
-    print("author first commit:")
+    print("author first commit: %s" % first_commit)
+    print("author last commit: %s" % last_commit)
+    for i in range(0, len(used_branch)):
+        print('%s commit to branch %s %d times' % (author, used_branch[i], count_branch[i]) )
+    print('-----------------------------------------------------------')
