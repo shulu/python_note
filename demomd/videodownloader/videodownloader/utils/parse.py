076bd9af6a54264e52a1d5b9ef973a9df1c0b6c6
舒露 <shulubanggood>
2018-07-18 18:08:51 +0800

    shulu commit

--- demomd/videodownloader/videodownloader/utils/parse.py
+++ demomd/videodownloader/videodownloader/utils/parse.py
@@ -2,3 +2,15 @@
 # -*- coding: utf-8 -*-
 
 __author__ = 'SarcasMe'
+
+from pyquery import PyQuery
+
+def parse(response):
+
+    '''
+    #zimu_list_content_1 > table > tbody:nth-child(3) > tr:nth-child(1) > td:nth-child(1) > a
+    :param url:
+    :return:
+    '''
+    jpy = PyQuery(response.text)
+    jpy('#zimu_list_content_1 > table > tbody:nth-child(3) > tr:nth-child(1) > td:nth-child(1) > a')
