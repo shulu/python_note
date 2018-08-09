158516557860f8d855794ac67b06a5e8c414f3c9
舒露 <shulubanggood>
2018-08-09 18:00:49 +0800

    shulu commit

--- pyQtdemo/demo01.py
+++ pyQtdemo/demo01.py
@@ -4,5 +4,17 @@
 __author__ = 'SarcasMe'
 
 import sys
-from PyQt5 import QtCore
+from PyQt5.QtWidgets import QApplication, QWidget
 
+
+if __name__ == '__main__':
+
+    app = QApplication(sys.argv)
+
+    w = QWidget()
+    w.resize(250, 150)
+    w.move(300, 300)
+    w.setWindowTitle('Hello world')
+    w.show()
+
+    sys.exit(app.exec_())
---
+++ pyQtdemo/demo02.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'SarcasMe'
+
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtGui import QIcon
+
+
+class Example(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+
+    def initUI(self):
+
+        self.setGeometry(300, 300, 300, 220)
+        self.setWindowTitle('Icon')
+        self.setWindowIcon(QIcon('./icon/exec.png'))
+
+        self.show()
+
+
+if __name__ == '__main__':
+
+    app = QApplication(sys.argv)
+    ex = Example()
+    sys.exit(app.exec_())
