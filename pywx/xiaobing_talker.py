93b101a1723d2d47b435a25e81e447f0d5d95022
Administrator <qq961085397@163.com>
2019-03-26 16:55:48 +0800

    pywx
    Signed-off-by: Administrator <qq961085397@163.com>

--- pywx/xiaobing_talker.py
+++ pywx/xiaobing_talker.py
@@ -12,27 +12,31 @@ bot = Bot()
 bot.enable_puid('wxpy_puid.pkl')
 
 # 指定xiaodan
-xiaodan = bot.friends().search('王晓丹')[0] #e7907b07
-#lizelin = bot.friends().search('李泽霖')[0] #2672d0d1
+# xiaodan = bot.friends().search('王晓丹')[0] #e7907b07
+yiting = bot.friends().search('钟怡婷')[0] #ee560386
+tester_id = yiting.puid
+# lizelin = bot.friends().search('李泽霖')[0] #2672d0d1
 
 # 指定小冰
 xiaobing = bot.mps().search('小冰')[0] #fcbc04ef
-
+xb_id = xiaobing.puid
 # 查看他的 puid
 # print(xiaodan.puid)
 # print(lizelin.puid)
 # print(xiaobing.puid)
+# print(yiting.puid)
 
-xiaodan.send('hi 我们开始聊天咯')
+# yiting.send('hi 我们开始聊天咯')
 
-@bot.register([xiaodan, xiaobing])
+
+@bot.register([yiting, xiaobing])
 def auto_reply(msg):
     sender_id = msg.sender.puid
-    if sender_id == 'e7907b07':
+    if sender_id == tester_id:
         xiaobing.send(msg.text)
         # msg.forward(xiaobing)
-    elif sender_id == 'fcbc04ef':
-        xiaodan.send(msg.text)
+    elif sender_id == xb_id:
+        yiting.send(msg.text)
         # msg.forword(xiaodan)
 
 embed()
